0b1c565bc16a5a24b05109d4561d6fbab3595db8
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-05-21 17:35:46 +0300

    star rating

--- components/detail/CreateComment.tsx
+++ components/detail/CreateComment.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import {
     Icon, InputGroup, InputRightElement, Input, IconButton, Tooltip,
     Modal,
@@ -22,6 +22,29 @@ import { AiFillStar } from 'react-icons/ai'
 const CreateComment: FC = () => {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const givenStar = useRef(3)
+
+    const handleStar = (e: SVGElement) => {
+        givenStar.current = parseInt(e.dataset!.star!)
+        paintStar(e)
+        unpaintStar(e)
+    }
+
+    const paintStar = (e: SVGElement) => {
+        e.style.color = '#006994'
+
+        if (e.previousElementSibling) {
+            paintStar(e.previousElementSibling as SVGElement)
+        }
+    }
+
+    const unpaintStar = (e: SVGElement) => {
+        if (e.nextElementSibling) {
+            const element = e.nextElementSibling as SVGElement
+            element.style.color = '#63b3ed'
+            unpaintStar(element)
+        }
+    }
 
     return <>
         <Modal isOpen={isOpen} onClose={onClose} size='xl'>
@@ -36,11 +59,11 @@ const CreateComment: FC = () => {
                             <Input bg='#eee' id='name' type='text' />
                         </FormControl>
                         <HStack flex={1} alignSelf='flex-end' pb={2}>
-                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
-                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
-                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
-                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
-                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon data-star={1} onMouseOver={(e) => handleStar(e.currentTarget)} as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon data-star={2} onMouseOver={(e) => handleStar(e.currentTarget)} as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon data-star={3} onMouseOver={(e) => handleStar(e.currentTarget)} as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon data-star={4} onMouseOver={(e) => handleStar(e.currentTarget)} as={AiFillStar} color={"blue.300"} fontSize={'2.5rem'} />
+                            <Icon data-star={5} onMouseOver={(e) => handleStar(e.currentTarget)} as={AiFillStar} color={"blue.300"} fontSize={'2.5rem'} />
                         </HStack>
                     </HStack>
                     <FormControl marginTop={5} isInvalid={true} isRequired={true}>
