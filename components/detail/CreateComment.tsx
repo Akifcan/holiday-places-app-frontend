a5c5fa057daac365ba21e16a4e4a4801b3987c05
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-05-21 13:53:05 +0300

    create comment modal

--- components/detail/CreateComment.tsx
+++ components/detail/CreateComment.tsx
@@ -1,16 +1,72 @@
 import { FC } from 'react'
-import { Icon, InputGroup, InputRightElement, Input, IconButton, Tooltip } from '@chakra-ui/react'
+import {
+    Icon, InputGroup, InputRightElement, Input, IconButton, Tooltip,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    HStack,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    Textarea
+} from '@chakra-ui/react'
 import { GrSend } from 'react-icons/gr'
+import { AiFillStar } from 'react-icons/ai'
 
 const CreateComment: FC = () => {
-    return <InputGroup bg='white' borderRadius={'md'}>
-        <Input shadow='lg' placeholder='Düşünceleriniz...' />
-        <InputRightElement>
-            <Tooltip label='Yorumunuzu Paylaşın'>
-                <IconButton aria-label='Yorumunuzu Paylaışın' onClick={() => true} icon={<Icon as={GrSend} />} />
-            </Tooltip>
-        </InputRightElement>
-    </InputGroup>
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return <>
+        <Modal isOpen={isOpen} onClose={onClose} size='xl'>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Yorumunuzu Paylaşın</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <HStack>
+                        <FormControl isRequired={true}>
+                            <FormLabel htmlFor='name'>Adınız</FormLabel>
+                            <Input bg='#eee' id='name' type='text' />
+                        </FormControl>
+                        <HStack flex={1} alignSelf='flex-end' pb={2}>
+                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                            <Icon as={AiFillStar} color={"primary"} fontSize={'2.5rem'} />
+                        </HStack>
+                    </HStack>
+                    <FormControl marginTop={5} isInvalid={true} isRequired={true}>
+                        <FormLabel htmlFor='comment'>Yorumunuz</FormLabel>
+                        <Textarea id='comment' bg='#eee' placeholder='Düşünceleriniz...' />
+                        <FormErrorMessage>Lütfen bu alanı boş bırakmayın</FormErrorMessage>
+                    </FormControl>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button variant={'ghost'} mr={3} onClick={onClose}>
+                        Geri Dön
+                    </Button>
+                    <Button colorScheme={'blue'} bg='primary'>Paylaş</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+        <InputGroup bg='white' borderRadius={'md'}>
+            <Input shadow='lg' placeholder='Düşünceleriniz...' />
+            <InputRightElement>
+                <Tooltip label='Yorumunuzu Paylaşın'>
+                    <IconButton aria-label='Yorumunuzu Paylaışın' onClick={onOpen} icon={<Icon as={GrSend} />} />
+                </Tooltip>
+            </InputRightElement>
+        </InputGroup>
+    </>
 }
 
 export default CreateComment
