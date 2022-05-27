760ec1e7f97722dd2565f2251b709c36e6b47926
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-05-27 10:55:37 +0300

    added timeago

--- components/detail/CommentCard.tsx
+++ components/detail/CommentCard.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react'
 import { Avatar, HStack, Text, VStack, Box } from '@chakra-ui/react'
 import { CommentQueryProps } from '@/apollo/quaries/comments'
-
+import TimeAgo from 'timeago-react'
+import * as timeago from 'timeago.js'
+import tr from 'timeago.js/lib/lang/tr'
+timeago.register('tr', tr)
 interface CommentCardProps {
     comment: CommentQueryProps
 }
 
 const CommentCard: FC<CommentCardProps> = ({ comment }) => {
-    console.log(comment)
-    return <VStack bg='white' p={5} borderRadius='md' alignItems={'flex-start'} shadow='lg'>
+    return <VStack bg='white' p={5} borderRadius='md' alignItems={'flex-start'} shadow='lg' textTransform={'capitalize'}>
         <HStack>
             <Avatar src={comment.profilePhoto} />
             <Box>
                 <Text fontWeight={'bold'}>{comment.username}</Text>
-                <Text as='time'>5 Gün Önce - {comment.rate} Yıldız Verdi</Text>
+                <Text as='time'><Text as='span'><TimeAgo
+                    datetime={comment.createdAt}
+                    locale='tr'
+                /></Text> - {comment.rate} Yıldız Verdi</Text>
             </Box>
         </HStack>
         <Text>{comment.body}</Text>
